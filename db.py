a86a362b31328f40922dd7dfee63da4332289c55
Rhanyele Marinho <rhany.marinho@hotmail.com>
2024-04-15 17:38:55 -0400

    Conexão com base de dados
    Configuração do banco de dados PostgreSQL e conexão via psycopg2

---
+++ db.py
@@ -0,0 +1,23 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+
+
+def connect_db():
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS
+    )
+    return conn
